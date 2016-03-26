7c9e21a742786b2e0e42e38672696d53efd30f2a
trueinviso <trueinviso@gmail.com>
2016-03-26 16:05:11 -0700

    refactor

--- src/Validations.java
+++ src/Validations.java
@@ -6,10 +6,6 @@ public class Validations {
     return str.matches("[-+]?[0-9]*\\.?[0-9]+");           
   }
 
-  public boolean isValidSymbol(String str) {
-    return str.matches("[\\+\\*-/]");
-  }
-
   public boolean quit(String str) {
     return !("q".indexOf(str) == -1); 
   }
